9c8e934863865c2324ef949c7b6326cbb227d0e4
Glitch (glitch-blank-react) <none>
2021-07-29 22:38:38 +0000

    🎸🐡 Checkpoint ./src/app.jsx:16231546/2164 ./src/index.css:16231546/1806 ./src/index.jsx:16231546/596

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.jsx";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://vitejs.dev/guide/api-hmr.html
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
